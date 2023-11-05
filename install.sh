7dba589ddaba17c99f7e7e5cc517c1b718445c1c
stebens1104 <stebens@amazon.com>
2023-11-05 15:28:32 -0500

    updated the install script

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 sudo apt install -y i3-wm xorg xbacklight xbindkeys xvkbd xinput xorg-dev
 sudo apt install -y python3-pip gh cups
 sudo apt install -y intel-microcode network-manager-gnome lxappearance thunar xfce4-settings xfce4-power-manager acpi acpid
-sudo apt install -y kitty pulseaudio alsa-utils pavucontrol volumeicon-alsa neofetch htop exa bluz blueman 
+sudo apt install -y kitty pulseaudio alsa-utils pavucontrol volumeicon-alsa neofetch htop exa bluez blueman 
 sudo apt install -y firefox-esr nitrogen picom 
 sudo apt install -y curl wget dmenu rofi dunst libnotify-bin lxpolkit unzip neovim gpg
 sudo apt install -y fonts-font-awesome fonts-powerline fonts-ubuntu-title fonts-liberation2 fonts-liberation fonts-terminus 
@@ -31,28 +31,6 @@ sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
 sudo apt update && sudo apt install -y code
-
-mkdir $HOME/.local/share/fonts
-
-fonts=( 
-"FiraCode" 
-"Go-Mono" 
-"Hack" 
-"Inconsolata" 
-"Iosevka" 
-"JetBrainsMono" 
-"Mononoki" 
-"RobotoMono" 
-"SourceCodePro" 
-)
-for font in ${fonts[@]}
-do
-    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/$font.zip
-    unzip $font.zip -d $HOME/.local/share/fonts/$font/
-    rm $font.zip
-done
-fc-cache
-
 
 # Install ZSH and plugins 
 sudo apt install -y zsh
