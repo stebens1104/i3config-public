8477ded76800d8b4184db37388691e4cf1f26d1d
stebens1104 <stebens@amazon.com>
2023-11-11 10:47:46 -0500

    updated install script

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
-
-mkdir -p $HOME/.local/share/fonts
-mkdir -p $HOME/,local/share/themes
+mkdir -p /home/$USERNAME/.local/share/fonts
+mkdir -p /home/$USERNAME/.local/share/themes
 
 # Installl pacakges needed for the configuration and corresponding .config folders
 
-sudo apt install -y i3-wm xorg xbacklight xbindkeys xvkbd xinput xorg-dev x11-server-utils
+sudo apt install -y i3-wm xorg xbacklight xbindkeys xvkbd xinput xorg-dev libu2f-udev 
 sudo apt install -y python3-pip gh cups
 sudo apt install -y intel-microcode network-manager-gnome lxappearance thunar xfce4-settings xfce4-power-manager acpi xfce4-terminal
 sudo apt install -y kitty pulseaudio alsa-utils pavucontrol volumeicon-alsa neofetch htop exa bluez blueman 
@@ -23,10 +22,10 @@ sudo systemctl enable cups
 xdg-user-dirs-update
 
 # Install Google Chrome
-cd $HOME/Downloads/
+cd /home/$USERNAME/Downloads/
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt install --fix-broken -y
+
 
 # Install VS Code
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
@@ -36,13 +35,13 @@ rm -f packages.microsoft.gpg
 sudo apt update && sudo apt install -y code
 
 # Install nerd fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Iosevka.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.3/Iosevka.zip
 sudo unzip Iosevka.zip -d /usr/share/fonts/
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
 sudo unzip JetBrainsMono.zip -d /usr/share/fonts/ 
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraCode.zip
 sudo unzip FiraCode.zip -d /usr/share/fonts/
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/IosevkaTerm.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/IosevkaTerm.zip
 sudo unzip IosevkaTerm.zip -d /usr/share/fonts/
 
 #Install nordic theme
